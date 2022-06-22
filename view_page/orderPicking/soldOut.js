0c2d72429776eefbfa18c2e7d6db335dfa6102ba
Allencl <Allen__cc@163.com>
2022-06-22 16:56:23 +0800

    1

--- view_page/orderPicking/soldOut.js
+++ view_page/orderPicking/soldOut.js
@@ -383,7 +383,7 @@ class Page extends Component {
           RequestURL="wms/pickingTask/list"
           Parames={{taskStatus:10}}
           onRef={(ref)=>{ this.tableRef=ref }}
-          maxHeight={height-346}
+          maxHeight={height-348}
           onInitHandle={(result)=>{
             this.setState({pageTotal:result.total})
             // console.log(result.total)
